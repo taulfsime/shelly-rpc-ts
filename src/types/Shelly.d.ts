98e39d2185137fe359894b5b982a2e4f3e98c777
taulfsime <kris.td@icloud.com>
2025-04-01 23:34:06 +0300

    add rpc and shelly rpc methods types

---
+++ src/types/Shelly.d.ts
@@ -0,0 +1,207 @@
+import {
+  shelly_component_id_t,
+  shelly_component_key_t,
+  shelly_component_type_t,
+  shelly_rpc_method_t,
+} from './ShellyRpc';
+
+export type shelly_device_info_data_t = {
+  name: string | null;
+  id: shelly_device_id_t;
+  mac: string;
+  slot: number;
+  model: string;
+  gen: 2 | 3 | 4;
+  fw_id: string;
+  ver: string;
+  app: string;
+  auth_en: boolean;
+  auth_domain: string | null;
+  discoverable?: boolean;
+  profile?: shelly_device_profile_t;
+  key?: string;
+  batch?: string;
+  fw_sbits?: string;
+  jti?: string;
+  jwt?: {
+    aud: string;
+    iat: number;
+    jti: string;
+    v: number;
+    p: string;
+    n: string;
+    url: string;
+    f: 0 | 1;
+  };
+} & {
+  [k: `svc${shelly_component_id_t}`]: {
+    type: string;
+    ver: string;
+    build_id: string;
+  };
+};
+
+export type shelly_device_profile_t = string;
+
+export type shelly_device_timezone_t = string;
+
+export type shelly_device_update_stage_t = 'beta' | 'stable' | 'test';
+export type shelly_device_update_info_t = {
+  version: string;
+  build_id: string;
+};
+
+export type shelly_device_id_t = string;
+export type shelly_device_auth_user_t = 'admin';
+export type shelly_device_auth_realm_t = shelly_device_id_t;
+export type shelly_device_auth_ha1_t = string | null;
+
+export type shelly_device_rpc_method_map_t = {
+  'Shelly.GetStatus': {
+    params: never;
+    result: never;
+  };
+  'Shelly.GetConfig': {
+    params: never;
+    result: never;
+  };
+  'Shelly.SetConfig': {
+    params: never;
+    result: never;
+  };
+  'Shelly.GetDeviceInfo': {
+    params?: {
+      ident?: boolean;
+    };
+    result: shelly_device_info_data_t;
+  };
+  'Shelly.ListMethods': {
+    params: never;
+    result: {
+      methods: shelly_rpc_method_t[];
+    };
+  };
+  'Shelly.ListProfiles': {
+    params: never;
+    result: {
+      profiles: {
+        [key: shelly_device_profile_t]: {
+          components: {
+            type: shelly_component_type_t;
+            count: number;
+          }[];
+        };
+      };
+    };
+  };
+  'Shelly.SetProfile': {
+    params: {
+      name: shelly_device_profile_t;
+    };
+    result: {
+      profile_was: shelly_device_profile_t;
+    };
+  };
+  'Shelly.ListTimezones': {
+    params?: {
+      offset?: number;
+    };
+    result: {
+      timezones: shelly_device_timezone_t[];
+      offset: number;
+      total: number;
+    };
+  };
+  'Shelly.DetectLocation': {
+    params: never;
+    result: {
+      tz: shelly_device_timezone_t | null;
+      lat: number | null;
+      lon: number | null;
+    };
+  };
+  'Shelly.CheckForUpdate': {
+    params: never;
+    result: Partial<
+      Record<shelly_device_update_stage_t, shelly_device_update_info_t>
+    >;
+  };
+  'Shelly.Update': {
+    params:
+      | {
+          stage: shelly_device_update_stage_t;
+        }
+      | {
+          url: string;
+        };
+    result: null;
+  };
+  'Shelly.FactoryReset': {
+    params: never;
+    result: null;
+  };
+  'Shelly.ResetWiFiConfig': {
+    params: never;
+    result: null;
+  };
+  'Shelly.Reboot': {
+    params?: {
+      delay_ms?: number;
+    };
+    result: null;
+  };
+  'Shelly.SetAuth': {
+    params: {
+      user: shelly_device_auth_user_t;
+      realm: shelly_device_auth_realm_t;
+      ha1: shelly_device_auth_ha1_t;
+    };
+    result: null;
+  };
+  'Shelly.PutUserCA': {
+    params: {
+      data: string | null;
+      append: boolean;
+    };
+    result: {
+      len: number;
+    };
+  };
+  'Shelly.PutTLSClientCert': {
+    params: {
+      data: string | null;
+      append: boolean;
+    };
+    result: {
+      len: number;
+    };
+  };
+  'Shelly.PutTLSClientKey': {
+    params: {
+      data: string | null;
+      append: boolean;
+    };
+    result: {
+      len: number;
+    };
+  };
+  'Shelly.GetComponents': {
+    params?: {
+      offset?: number;
+      include?: ('status' | 'config' | 'attrs')[];
+      keys?: shelly_component_key_t[];
+      dynamic_only?: boolean;
+    };
+    result: {
+      components: {
+        key: shelly_component_key_t;
+        status?: any; //XXX: Change to specific types
+        config?: any; //XXX: Change to specific types
+        attrs?: any; //XXX: Change to specific types
+      }[];
+      cfg_rev: number;
+      offset: number;
+      total: number;
+    };
+  };
+};
---
+++ src/types/ShellyRpc.d.ts
@@ -0,0 +1,112 @@
+import { shelly_device_rpc_method_map_t } from './Shelly';
+
+export type shelly_rpc_msg_out_t = {
+  id: number;
+  src: string;
+  method: shelly_rpc_method_t;
+  params?: Record<string, unknown>; //XXX: Change to specific types
+};
+
+type shelly_rpc_notification_event_t = {
+  dst: string;
+  src: string;
+  method: 'NotifyEvent';
+  params: {
+    ts: number;
+    events: {
+      component: shelly_component_key_t;
+      id?: shelly_component_id_t;
+      event: string; //XXX: Change to specific types
+      ts: number;
+    }[];
+  };
+};
+
+type shelly_rpc_notification_status_t = {
+  dst: string;
+  src: string;
+  method: 'NotifyStatus';
+  params: {
+    ts: number;
+  } & {
+    [key in shelly_component_key_t]?: shelly_rpc_method_result_t<shelly_rpc_method_t>;
+  };
+};
+
+export type shelly_rpc_notification_t =
+  | shelly_rpc_notification_event_t
+  | shelly_rpc_notification_status_t;
+
+type shelly_component_single_instance_t = 'sys' | 'wifi' | 'mqtt';
+
+type shelly_component_multi_instance_t =
+  | 'switch'
+  | 'cover'
+  | 'light'
+  | 'service'
+  | 'number'
+  | 'boolean'
+  | 'text'
+  | 'object'
+  | 'enum'
+  | 'script'
+  | 'temperature'
+  | 'humidity'
+  | 'input'
+  | 'bthomesensor'
+  | 'bthomedevice'
+  | 'pm1';
+
+export type shelly_component_id_t = number;
+export type shelly_component_type_t =
+  | shelly_component_single_instance_t
+  | shelly_component_multi_instance_t;
+
+export type shelly_component_key_t =
+  | shelly_component_single_instance_t
+  | `${shelly_component_multi_instance_t}:${shelly_component_id_t}`;
+
+type shelly_rpc_method_map_t = shelly_device_rpc_method_map_t;
+
+type shelly_rpc_method_t = keyof shelly_rpc_method_map_t;
+
+type shelly_rpc_method_params_t<K extends shelly_rpc_method_t> =
+  K extends keyof shelly_rpc_method_map_t
+    ? shelly_rpc_method_map_t[K] extends { params: infer P }
+      ? P
+      : shelly_rpc_method_map_t[K] extends { params?: infer P }
+        ? P | undefined
+        : never
+    : never;
+
+export type shelly_rpc_method_result_t<K extends shelly_rpc_method_t> =
+  K extends keyof shelly_rpc_method_map_t
+    ? shelly_rpc_method_map_t[K]['result']
+    : null;
+
+export type shelly_rpc_request_t<K extends shelly_rpc_method_t> = {
+  jsonrpc: '2.0';
+  id: number;
+  method: K;
+} & ({} extends shelly_rpc_method_params_t<K>
+  ? { params?: shelly_rpc_method_params_t<K> }
+  : shelly_rpc_method_params_t<K> extends undefined | never
+    ? { params?: shelly_rpc_method_params_t<K> }
+    : { params: shelly_rpc_method_params_t<K> });
+
+export type shelly_rpc_method_response_t<K extends shelly_rpc_method_t> =
+  | {
+      id: number;
+      src: string;
+      dst: string;
+      result: shelly_rpc_method_result_t<K>;
+    }
+  | {
+      id: number;
+      src: string;
+      dst: string;
+      error: {
+        code: number;
+        message: string;
+      };
+    };
