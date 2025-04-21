3815a60b76687f4096554c9e64325d6531c293ce
taulfsime <kris.td@icloud.com>
2025-04-21 21:17:47 +0300

    complete the wifi component methods

--- src/components/WiFi.d.ts
+++ src/components/WiFi.d.ts
@@ -44,14 +44,20 @@ export type shelly_wifi_status_t = {
   ap_client_count: number;
 };
 
-export type shelly_wifi_scan_result_t = {
-  results: {
-    ssid: string | null;
-    bssid: string;
-    rssi: number;
-    channel: number;
-    auth: 0 | 1 | 2 | 3 | 4 | 5; // https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/WiFi#wifiscan
-  }[];
+export type shelly_wifi_scan_single_result_t = {
+  ssid: string | null;
+  bssid: string;
+  rssi: number;
+  channel: number;
+  auth: 0 | 1 | 2 | 3 | 4 | 5; // https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/WiFi#wifiscan
+};
+
+export type shelly_wifi_ap_client_single_result_t = {
+  mac: shelly_device_mac_t; //XXX:
+  ip: string;
+  ip_static: boolean;
+  mport: number;
+  since: number;
 };
 
 export type shelly_wifi_rpc_method_map_t = {
@@ -62,5 +68,26 @@ export type shelly_wifi_rpc_method_map_t = {
   'WiFi.GetConfig': {
     params: never;
     result: shelly_wifi_config_t;
+  };
+  'WiFi.SetConfig': {
+    params: {
+      config: shelly_wifi_config_t;
+    };
+    result: {
+      restart_required: boolean;
+    };
+  };
+  'WiFi.Scan': {
+    params: never;
+    result: {
+      results: shelly_wifi_scan_single_result_t[];
+    };
+  };
+  'WiFi.ListAPClients': {
+    params: never;
+    result: {
+      ts: number | null;
+      ap_clients: shelly_wifi_ap_client_single_result_t[];
+    };
   };
 };
