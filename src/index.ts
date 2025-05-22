bb689c6b4a0c4fc914a9b9cefbe2e7e32081d63b
taulfsime <kris.td@icloud.com>
2025-05-22 08:24:03 +0300

    expose component keys from the package
    Signed-off-by: taulfsime <kris.td@icloud.com>

--- src/index.ts
+++ src/index.ts
@@ -3,124 +3,212 @@ export { shelly_sys_config_t, shelly_sys_status_t } from './components/Sys.js';
 export {
   shelly_wifi_config_t,
   shelly_wifi_status_t,
+  shelly_wifi_key_t,
+  shelly_wifi_type_t,
 } from './components/WiFi.js';
 export {
   shelly_mqtt_config_t,
   shelly_mqtt_status_t,
+  shelly_mqtt_key_t,
+  shelly_mqtt_type_t,
 } from './components/MQTT.js';
 export {
   shelly_switch_config_t,
   shelly_switch_status_t,
+  shelly_switch_key_t,
+  shelly_switch_type_t,
 } from './components/Switch.js';
 export {
   shelly_cover_config_t,
   shelly_cover_status_t,
+  shelly_cover_key_t,
+  shelly_cover_type_t,
 } from './components/Cover.js';
 export {
   shelly_light_config_t,
   shelly_light_status_t,
+  shelly_light_key_t,
+  shelly_light_type_t,
 } from './components/Light.js';
 export {
   shelly_service_config_t,
   shelly_service_status_t,
+  shelly_service_key_t,
+  shelly_service_type_t,
 } from './components/Service.js';
 export {
   shelly_number_config_t,
   shelly_number_status_t,
+  shelly_number_key_t,
+  shelly_number_type_t,
 } from './components/VirtualComponents/Number.js';
 export {
   shelly_boolean_config_t,
   shelly_boolean_status_t,
+  shelly_boolean_key_t,
+  shelly_boolean_type_t,
 } from './components/VirtualComponents/Boolean.js';
 export {
   shelly_text_config_t,
   shelly_text_status_t,
+  shelly_text_key_t,
+  shelly_text_type_t,
 } from './components/VirtualComponents/Text.js';
 export {
   shelly_enum_config_t,
   shelly_enum_status_t,
+  shelly_enum_key_t,
+  shelly_enum_type_t,
 } from './components/VirtualComponents/Enum.js';
 export {
   shelly_button_config_t,
   shelly_button_status_t,
+  shelly_button_type_t,
+  shelly_button_key_t,
 } from './components/VirtualComponents/Button.js';
 export {
   shelly_group_config_t,
   shelly_group_status_t,
+  shelly_group_type_t,
+  shelly_group_key_t,
 } from './components/VirtualComponents/Group.js';
 export {
   shelly_script_config_t,
   shelly_script_status_t,
+  shelly_script_type_t,
+  shelly_script_key_t,
 } from './components/Script.js';
 export {
   shelly_temperature_config_t,
   shelly_temperature_status_t,
+  shelly_temperature_key_t,
+  shelly_temperature_type_t,
 } from './components/Temperature.js';
 export {
   shelly_humidity_config_t,
   shelly_humidity_status_t,
+  shelly_humidity_key_t,
+  shelly_humidity_type_t,
 } from './components/Humidity.js';
 export {
   shelly_input_config_t,
   shelly_input_status_t,
+  shelly_input_key_t,
+  shelly_input_type_t,
 } from './components/Input.js';
 export {
   shelly_bthomesensor_config_t,
   shelly_bthomesensor_status_t,
+  shelly_bthomesensor_key_t,
+  shelly_bthomesensor_type_t,
 } from './components/BTHomeComponents/BTHomeSensor.js';
 export {
   shelly_bthomedevice_config_t,
   shelly_bthomedevice_status_t,
+  shelly_bthomedevice_key_t,
+  shelly_bthomedevice_type_t,
 } from './components/BTHomeComponents/BTHomeDevice.js';
 export {
   shelly_object_config_t,
   shelly_object_status_t,
+  shelly_object_key_t,
+  shelly_object_type_t,
 } from './components/VirtualComponents/Object.js';
-export { shelly_pm1_config_t, shelly_pm1_status_t } from './components/PM1.js';
+export {
+  shelly_pm1_config_t,
+  shelly_pm1_status_t,
+  shelly_pm1_key_t,
+  shelly_pm1_type_t,
+} from './components/PM1.js';
 export {
   shelly_devicepower_config_t,
   shelly_devicepower_status_t,
+  shelly_devicepower_key_t,
+  shelly_devicepower_type_t,
 } from './components/DevicePower.js';
 export {
   shelly_ethernet_config_t,
   shelly_ethernet_status_t,
+  shelly_ethernet_key_t,
+  shelly_ethernet_type_t,
 } from './components/Ethernet.js';
-export { shelly_ble_config_t, shelly_ble_status_t } from './components/BLE.js';
+export {
+  shelly_ble_config_t,
+  shelly_ble_status_t,
+  shelly_ble_key_t,
+  shelly_ble_type_t,
+} from './components/BLE.js';
 export {
   shelly_cloud_config_t,
   shelly_cloud_status_t,
+  shelly_cloud_key_t,
+  shelly_cloud_type_t,
 } from './components/Cloud.js';
-export { shelly_ws_config_t, shelly_ws_status_t } from './components/WS.js';
+export {
+  shelly_ws_config_t,
+  shelly_ws_status_t,
+  shelly_ws_key_t,
+  shelly_ws_type_t,
+} from './components/WS.js';
 export {
   shelly_matter_config_t,
   shelly_matter_status_t,
+  shelly_matter_key_t,
+  shelly_matter_type_t,
 } from './components/Matter.js';
 export {
   shelly_modbus_status_t,
   shelly_modbus_config_t,
+  shelly_modbus_key_t,
+  shelly_modbus_type_t,
 } from './components/Modbus.js';
 export {
   shelly_voltmeter_config_t,
   shelly_voltmeter_status_t,
+  shelly_voltmeter_key_t,
+  shelly_voltmeter_type_t,
 } from './components/Voltmeter.js';
 export {
   shelly_smoke_status_t,
   shelly_smoke_config_t,
+  shelly_smoke_key_t,
+  shelly_smoke_type_t,
 } from './components/Smoke.js';
 export {
   shelly_dali_status_t,
   shelly_dali_config_t,
+  shelly_dali_key_t,
+  shelly_dali_type_t,
 } from './components/DALI.js';
-export { shelly_cct_status_t, shelly_cct_config_t } from './components/CCT.js';
+export {
+  shelly_cct_status_t,
+  shelly_cct_config_t,
+  shelly_cct_key_t,
+  shelly_cct_type_t,
+} from './components/CCT.js';
 export {
   shelly_zigbee_status_t,
   shelly_zigbee_config_t,
+  shelly_zigbee_key_t,
+  shelly_zigbee_type_t,
 } from './components/Zigbee.js';
-export { shelly_em_status_t, shelly_em_config_t } from './components/EM.js';
-export { shelly_em1_status_t, shelly_em1_config_t } from './components/EM1.js';
+export {
+  shelly_em_status_t,
+  shelly_em_config_t,
+  shelly_em_key_t,
+  shelly_em_type_t,
+} from './components/EM.js';
+export {
+  shelly_em1_status_t,
+  shelly_em1_config_t,
+  shelly_em1_key_t,
+  shelly_em1_type_t,
+} from './components/EM1.js';
 export {
   shelly_emdata_status_t,
   shelly_emdata_config_t,
+  shelly_emdata_key_t,
+  shelly_emdata_type_t,
 } from './components/EMData.js';
 export { shelly_schedule_job_t } from './components/Schedule.js';
 export { shelly_webhook_hook_t } from './components/Webhook.js';
@@ -128,19 +216,35 @@ export { shelly_knx_input_config_t } from './components/KNXComponents/KNXInput.j
 export { shelly_knx_switch_config_t } from './components/KNXComponents/KNXSwitch.js';
 export { shelly_knx_light_config_t } from './components/KNXComponents/KNXLight.js';
 export { shelly_knx_cover_config_t } from './components/KNXComponents/KNXCover.js';
-export { shelly_knx_config_t, shelly_knx_status_t } from './components/KNX.js';
+export {
+  shelly_knx_config_t,
+  shelly_knx_status_t,
+  shelly_knx_key_t,
+  shelly_knx_type_t,
+} from './components/KNX.js';
 export {
   shelly_bthome_status_t,
   shelly_bthome_config_t,
+  shelly_bthome_key_t,
+  shelly_bthome_type_t,
 } from './components/BTHome.js';
 export {
   shelly_em1data_status_t,
   shelly_em1data_config_t,
+  shelly_em1data_key_t,
+  shelly_em1data_type_t,
 } from './components/EM1Data.js';
-export { shelly_rgb_status_t, shelly_rgb_config_t } from './components/RGB.js';
+export {
+  shelly_rgb_status_t,
+  shelly_rgb_config_t,
+  shelly_rgb_key_t,
+  shelly_rgb_type_t,
+} from './components/RGB.js';
 export {
   shelly_rgbw_status_t,
   shelly_rgbw_config_t,
+  shelly_rgbw_key_t,
+  shelly_rgbw_type_t,
 } from './components/RGBW.js';
 export {
   shelly_virtual_component_key_t,
