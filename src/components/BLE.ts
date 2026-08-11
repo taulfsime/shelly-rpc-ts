e12845c8a08ef5c398b85d233f1791685845cd5e
i.vasilev <iliyan.vasilev@shelly.com>
2026-08-11 11:39:23 +0300

    BLE type update

--- src/components/BLE.ts
+++ src/components/BLE.ts
@@ -4,13 +4,21 @@ export type shelly_ble_type_t = 'ble';
 export type shelly_ble_key_t = shelly_ble_type_t;
 
 export type shelly_ble_config_t = {
-  enable: boolean;
   rpc: {
     enable: boolean;
   };
+  keep_running?: boolean;
 };
 
+type shelly_ble_status_flag_t = 'scanning' | 'advertising' | 'connected';
+
 export type shelly_ble_status_t = {
+  addr: string;
+  flags?: shelly_ble_status_flag_t[];
+  pairing?: {
+    started_at: number;
+    duration: number;
+  };
   blutrv_assoc?: {
     duration: number;
     started_at: number;
@@ -24,6 +32,14 @@ export type shelly_ble_client_single_result_t = {
   mdata: Record<string, string>;
   last_seen: number;
 };
+
+export type shelly_ble_paired_device_t = {
+  addr: string;
+  ctime: number;
+  atime: number;
+};
+
+export type shelly_ble_bthome_command_opcode_t = 0 | 1 | 2 | 3 | 4;
 
 export type shelly_ble_rpc_method_map_t = {
   'BLE.GetConfig': {
@@ -61,11 +77,57 @@ export type shelly_ble_rpc_method_map_t = {
       devices: shelly_ble_client_single_result_t[];
     };
   };
+  'BLE.StartAssociations': {
+    params: {
+      target: number;
+      param?: unknown;
+      duration?: number;
+      rssi_thr?: number;
+    };
+    result: null;
+  };
   'BLE.StartBluTrvAssociations': {
     params: {
-      blutrv_id: number;
-      duration: number;
-      rssi_thr: number;
+      blutrv_id?: number;
+      duration?: number;
+      rssi_thr?: number;
+    };
+    result: null;
+  };
+  'BLE.StartPairing': {
+    params?: {
+      timeout?: number;
+    };
+    result: {
+      timeout: number;
+    };
+  };
+  'BLE.StopPairing': {
+    params?: {};
+    result: null;
+  };
+  'BLE.ListPairedDevices': {
+    params?: {};
+    result: shelly_ble_paired_device_t[];
+  };
+  'BLE.DeletePairedDevice': {
+    params: {
+      addr: string;
+    };
+    result: null;
+  };
+  'BLE.AdvertiseOnce': {
+    params: {
+      adv_data: string;
+      scan_rsp?: string;
+    };
+    result: null;
+  };
+  'BLE.SendBTHomeCommand': {
+    params: {
+      opcode: shelly_ble_bthome_command_opcode_t;
+      args?: string;
+      key: string;
     };
     result: null;
   };
